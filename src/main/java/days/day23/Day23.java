deec71940d0f67aae2e47e9ee2aaba874638d592
tommy <t.oosterwijk@flusso.nl>
2020-12-26 13:32:25 +0100

    rework

--- src/main/java/days/day23/Day23.java
+++ src/main/java/days/day23/Day23.java
@@ -3,127 +3,107 @@ package days.day23;
 import java.util.ArrayList;
 
 public class Day23 {
+    //String entry = "389125467";
     String entry = "247819356";
-    int maxValue = 9;
-    ArrayList<Integer> cubs = new ArrayList<>();
+    //Integer[] leftSide = new Integer[1000001];
+    Integer[] rightSide = new Integer[1000001];
+    int playValue = 0;
 
+    //Day23B = 12621748849
+    //time = 3228078256
 
     public Day23() {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
 
-        for(int i = 0; i < entry.length(); i++) {
-            cubs.add(Integer.parseInt(entry.charAt(i) + ""));
+        // this will stay blank
+        //leftSide[0] = 0;
+        rightSide[0] = 0;
+
+        playValue = Integer.parseInt(entry.charAt(0) + "");
+        int previousValue = Integer.parseInt(entry.charAt(0) + "");
+        for(int i = 1; i < entry.length(); i++) {
+            //leftSide[Integer.parseInt(entry.charAt(i) + "")] = previousValue;
+            rightSide[previousValue] = Integer.parseInt(entry.charAt(i) + "");
+            previousValue = Integer.parseInt(entry.charAt(i) + "");
         }
-//        for(int i = 10; i <= 100; i++) {
-//            cubs.add(i);
-//        }
-        maxValue = cubs.size();
+
+        for(int i = 10; i <= 1000000; i++) {
+            //leftSide[i] = previousValue;
+            rightSide[previousValue] = i;
+            previousValue = i;
+        }
+        //leftSide[playValue] = previousValue;
+        rightSide[previousValue] = playValue;
+
         playGame();
-        new Day23Rework2();
     }
 
     private void playGame() {
-        int index = 0;
         long startTime = System.nanoTime();
 
-        for(int i = 1; i <= 100; i++) {
-            int currentCub = cubs.get(index);
-            printCubsStack();
-            //System.out.println("Current cub = " + currentCub);
-            ArrayList<Integer> roundCubs = new ArrayList<>();
-            roundCubs.add(cubs.get(getValidIndex(index+1)));
-            roundCubs.add(cubs.get(getValidIndex(index+2)));
-            roundCubs.add(cubs.get(getValidIndex(index+3)));
-            //System.out.println("pick up: " + roundCubs);
+        for(int i = 1; i <= 10000000; i++) {
 
-            cubs.removeAll(roundCubs);
-//            System.out.println("cubs after removed = " + cubs);
-            for(int i2= 1; i2 < maxValue; i2++) {
-                int targetCub = getValidTargetValue(currentCub - i2);
-                if(targetCub < 1) {
-                    targetCub += maxValue;
-                }
+            //System.out.println("-- move " + i + " --");
 
-                if (cubs.contains(targetCub)) {
-                    for(int x = 0; x < cubs.size(); x++) {
-                        if(cubs.get(x) == targetCub) {
-                            if(x < index) {
-                                index += 3;
-                                break;
-                            }
-                        }
-                    }
-                    //System.out.println("Destination: " + targetCub);
-                    int targetIndex = cubs.indexOf(targetCub) + 1;
-                    cubs.addAll(targetIndex, roundCubs);
-                    int temp = 0;
+            //System.out.println("current cub: " +playValue);
+            int cup1 = rightSide[playValue];
+            int cup2 = rightSide[cup1];
+            int cup3 = rightSide[cup2];
 
-                    index = getIndexBasedOnValue(currentCub);
-                    break;
+            int destination = playValue - 1;
+            if(destination == 0) {
+                destination =rightSide.length-1;
+            }
+            while(destination == cup1 || destination == cup2 || destination == cup3) {
+                destination--;
+                if(destination == 0) {
+                    destination =rightSide.length-1;
                 }
             }
-            index++;
-            if(index >= cubs.size()) {
-                index -= cubs.size();
-            }
+
+//            System.out.println("Pickup: " + cup1 + ", " + cup2 + ", " + cup3);
+//            System.out.println("Destination: " + destination);
+
+            int rightSideCup3 = rightSide[cup3];
+            int leftSideCup1 = //leftSide[cup1];
+
+            rightSide[playValue]= rightSideCup3;
+            //leftSide[rightSideCup3] = leftSideCup1;
+
+            int endIndex = rightSide[destination];
+            rightSide[destination] = cup1;
+            //leftSide[cup1] = destination;
+
+            rightSide[cup3] = endIndex;
+            //leftSide[endIndex] = cup3;
+            playValue = rightSide[playValue];
         }
+
+
 
         //System.out.println("Final cubs " + cubs);
         //day23A();
-        long stopTime = System.nanoTime();
-        System.out.println(stopTime - startTime);
         day23B();
-
-        System.out.println(cubs);
-    }
-
-    private void day23B() {
-        int index = cubs.indexOf(1);
-        long value = 1;
-        value = cubs.get(index+1) *  cubs.get(index+2);
-        System.out.println("Day 23 B = "+ value);
+        long stopTime = System.nanoTime();
+        System.out.println("time = " + (stopTime - startTime));
     }
 
     private void day23A() {
-        int index = cubs.indexOf(1);
-
-        for(int i =1; i < maxValue; i++) {
-            System.out.print(cubs.get(getValidIndex(index + i)));
+        int value = rightSide[1];
+        for(int i =1; i < 9; i++) {
+            System.out.print(value);
+            value = rightSide[value];
         }
         System.out.println();
     }
 
-    private int getIndexBasedOnValue(int value) {
-        for(int i = 0; i < cubs.size(); i++) {
-            if(cubs.get(i) == value) {
-                return i;
-            }
-        }
-
-        return 0;
+    private void day23B() {
+        long value = rightSide[1];
+        value *= rightSide[rightSide[1]];
+        System.out.println("Day23B = " + value);
     }
 
-    private int getValidIndex(int index) {
 
-        if(index >= maxValue) {
-            index -= maxValue;
-        }
-
-        return index;
-    }
-
-    private int getValidTargetValue(int value) {
-        if( value == 0) {
-            value = maxValue;
-        }
-
-        return value;
-    }
-
-    private void printCubsStack() {
-        //System.out.println(cubs);
-    }
 }
