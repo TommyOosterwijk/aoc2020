a8ba771149371ac670f6abb541d784dc804e06db
tommy <t.oosterwijk@flusso.nl>
2020-12-26 13:20:35 +0100

    Rework

--- src/main/java/days/day13/Day13b.java
+++ src/main/java/days/day13/Day13b.java
@@ -54,80 +54,10 @@ public class Day13b {
                     break;
                 }
             }
-            LCM = lcm(LCM, Long.valueOf((bus.getId())));
+            LCM *= bus.getId();
         }
 
         System.out.println("Day13 B = " + departureTime);
 
-    }
-
-
-    long gcd(Long a, Long b)
-    {
-        if (a == 0)
-            return b;
-        return gcd(b % a, a);
-    }
-
-    long lcm(Long a, Long b)
-    {
-        return (a / gcd(a, b)) * b;
-    }
-
-    private void shuttleBruteForce(String line) {
-        for(String stringID : line.split(",")) {
-            provenValues.add(new BigDecimal(0));
-        }
-
-
-
-        Collections.sort(busList, Comparator.comparing(Bus::getId));
-        Collections.reverse(busList);
-
-        boolean valueNotFound = true;
-        BigDecimal firstValue = new BigDecimal(busList.get(0).getId());
-        //BigDecimal highestTargetValue = new BigDecimal(startIndex);
-        long counter = 0;
-        BigDecimal highestTargetValue = firstValue;
-        while(valueNotFound) {
-            highestTargetValue = highestTargetValue.add(firstValue);
-            BigDecimal lowestTargetValue = highestTargetValue;
-            counter++;
-            if(counter == 112931831l) {
-                System.out.println("Highest target to find = " + highestTargetValue);
-            counter = 0;
-            }
-            Boolean targetValueFound = true;
-
-            for( int  i = 1; i < busList.size(); i++) {
-                BigDecimal currentTarget = highestTargetValue.subtract(provenValues.get(i));
-                Bus bus = busList.get(i);
-//                System.out.println(busList.get(0).getIndex());
-//                System.out.println(busList.get(i).getIndex());
-//                System.out.println(busList.get(0).getIndex() - busList.get(i).getIndex());
-//                System.out.println(currentTarget);
-//                System.out.println("test " + currentTarget.subtract(new BigDecimal(3)));
-                currentTarget = currentTarget.subtract(new BigDecimal(busList.get(0).getIndex() - bus.getIndex()));
-                //System.out.println("New target for lower index = " + currentTarget);
-
-                BigDecimal remainder = currentTarget.remainder(new BigDecimal(bus.getId()));
-                if (BigDecimal.ZERO.compareTo(remainder) == 0) {
-                    BigDecimal realValue = currentTarget.add(provenValues.get(i));
-                    if(lowestTargetValue.compareTo(realValue) == 1) {
-                        lowestTargetValue = realValue;
-                    }
-                    provenValues.set(i, currentTarget);
-                    //System.out.println( currentTarget  + " can be divided by " + bus.getId());
-                } else {
-                    targetValueFound = false;
-                    break;
-                }
-            }
-
-            if( targetValueFound) {
-                System.out.println("Day13B = " + lowestTargetValue);
-                valueNotFound = false;
-            }
-        }
     }
 }
